14249506f342e8d4bc78f37056bec6f01f759cac
ai25395 <52650634+ai25395@users.noreply.github.com>
2024-10-21 10:30:01 +0800

    Add files via upload

---
+++ ocr_gui.py
@@ -0,0 +1,268 @@
+﻿from tkinter import scrolledtext
+from turtle import mode
+from win32 import win32api, win32gui, win32print
+from win32.lib import win32con
+from win32.win32api import GetSystemMetrics
+import tkinter as tk
+from PIL import ImageGrab, Image, ImageTk
+import pyperclip
+import latex2mathml.converter
+import models
+import utils
+
+def get_real_resolution():
+    hDC = win32gui.GetDC(0)
+    # 横向分辨率
+    w = win32print.GetDeviceCaps(hDC, win32con.DESKTOPHORZRES)
+    # 纵向分辨率
+    h = win32print.GetDeviceCaps(hDC, win32con.DESKTOPVERTRES)
+    return w, h
+
+
+import sys
+import os
+
+
+def get_screen_size():
+    # 获取缩放后的分辨率
+    w = GetSystemMetrics(0)
+    h = GetSystemMetrics(1)
+    return w, h
+
+
+real_resolution = get_real_resolution()
+screen_size = get_screen_size()
+# Windows 设置的屏幕缩放率
+# ImageGrab 的参数是基于显示分辨率的坐标，而 tkinter 获取到的是基于缩放后的分辨率的坐标
+screen_scale_rate = round(real_resolution[0] / screen_size[0], 2)
+
+
+class Box:
+
+    def __init__(self):
+        self.start_x = None
+        self.start_y = None
+        self.end_x = None
+        self.end_y = None
+
+    def isNone(self):
+        return self.start_x is None or self.end_x is None
+
+    def setStart(self, x, y):
+        self.start_x = x
+        self.start_y = y
+
+    def setEnd(self, x, y):
+        self.end_x = x
+        self.end_y = y
+
+    def box(self):
+        lt_x = min(self.start_x, self.end_x)
+        lt_y = min(self.start_y, self.end_y)
+        rb_x = max(self.start_x, self.end_x)
+        rb_y = max(self.start_y, self.end_y)
+        return lt_x, lt_y, rb_x, rb_y
+
+    def center(self):
+        center_x = (self.start_x + self.end_x) / 2
+        center_y = (self.start_y + self.end_y) / 2
+        return center_x, center_y
+
+
+class SelectionArea:
+
+    def __init__(self, canvas: tk.Canvas):
+        self.canvas = canvas
+        self.area_box = Box()
+
+    def empty(self):
+        return self.area_box.isNone()
+
+    def setStartPoint(self, x, y):
+        self.canvas.delete("area", "lt_txt", "rb_txt")
+        self.area_box.setStart(x, y)
+        self.canvas.create_text(x, y - 10, text=f"({x}, {y})", fill="red", tag="lt_txt")
+
+    def updateEndPoint(self, x, y):
+        self.area_box.setEnd(x, y)
+        self.canvas.delete("area", "rb_txt")
+        box_area = self.area_box.box()
+        # 画矩形区域
+        self.canvas.create_rectangle(
+            *box_area, fill="black", outline="red", width=2, tags="area"
+        )
+        self.canvas.create_text(x, y + 10, text=f"({x}, {y})", fill="red", tag="rb_txt")
+
+
+class ScreenShot:
+
+    def __init__(self, scaling_factor=2, main=None):
+        self.win = tk.Tk()
+        # self.win.tk.call('tk', 'scaling', scaling_factor)
+        self.width = self.win.winfo_screenwidth()
+        self.height = self.win.winfo_screenheight()
+        self.main = main
+        # 无边框，无法拖动的半透明窗口
+        self.win.overrideredirect(True)
+        self.win.attributes("-alpha", 0.25)
+
+        self.is_selecting = False
+
+        # Enter 确认, Esc 退出
+        self.win.bind("<KeyPress-Escape>", self.exit)
+        self.win.bind("<KeyPress-Return>", self.confirmScreenShot)
+        self.win.bind("<Button-1>", self.selectStart)
+        self.win.bind("<ButtonRelease-1>", self.selectDone)
+        self.win.bind("<Motion>", self.changeSelectionArea)
+
+        self.canvas = tk.Canvas(self.win, width=self.width, height=self.height)
+        self.canvas.pack()
+        self.area = SelectionArea(self.canvas)
+        self.win.mainloop()
+
+    def exit(self, event):
+        self.win.quit()
+        self.win.destroy()
+
+    def clear(self):
+        self.canvas.delete("area", "lt_txt", "rb_txt")
+        self.win.attributes("-alpha", 0)
+
+    def captureImage(self):
+        if self.area.empty():
+            return None
+        else:
+            box_area = [x * screen_scale_rate for x in self.area.area_box.box()]
+            self.clear()
+            print(f"Grab: {box_area}")
+            img = ImageGrab.grab(box_area)
+
+            return img
+
+    def confirmScreenShot(self, event):
+        img = self.captureImage()
+        self.main.image = img
+        self.win.quit()
+        self.win.destroy()
+
+    def selectStart(self, event):
+        self.is_selecting = True
+        self.area.setStartPoint(event.x, event.y)
+
+    def changeSelectionArea(self, event):
+        if self.is_selecting:
+            self.area.updateEndPoint(event.x, event.y)
+
+    def selectDone(self, event):
+        self.is_selecting = False
+
+
+class MainWindow:
+    def __init__(self, image_path=None, ocr=None):
+        self.root = tk.Tk()
+        # 固定窗口尺寸
+        if getattr(sys, "frozen", None):
+            basedir = sys._MEIPASS
+        else:
+            basedir = os.path.dirname(__file__)
+        self.root.iconbitmap(os.path.join(basedir, "models/guiicon.ico"))
+        self.root.resizable(0, 0)
+        self.ocr = ocr
+        self.mathtypetext = ""
+        self.latextext = ""
+        if image_path is None:
+            raise ValueError("Image path must be provided.")
+
+        self.root.title("Free Formula OCR for Latex and MathML -~- By ltc")
+        self.root.geometry("500x210")
+
+        # 加载并显示图片
+        self.image = Image.open(image_path)
+        self.image = self.image.resize((500, 125))
+        self.photo = ImageTk.PhotoImage(self.image)
+
+        self.image_label = tk.Label(self.root, image=self.photo)
+        self.image_label.image = self.photo  # 保持对图片的引用
+        self.image_label.pack(pady=0)
+
+        # 创建按钮框架和按钮
+        self.button_frame = tk.Frame(self.root)
+        self.button_frame.pack(fill=tk.X, padx=45, pady=5)
+
+        self.copy_latex_button = tk.Button(
+            self.button_frame,
+            text="Copy Latex",
+            width=12,
+            height=2,
+            command=self.on_copy_latex,
+        )
+        self.copy_latex_button.grid(row=0, column=0, padx=5, pady=5)
+
+        self.copy_mathtype_button = tk.Button(
+            self.button_frame,
+            text="Copy MathMl",
+            width=12,
+            height=2,
+            command=self.on_copy_mathtype,
+        )
+        self.copy_mathtype_button.grid(row=0, column=1, padx=5, pady=5)
+
+        self.snip_button = tk.Button(
+            self.button_frame, text="Snipping", width=12, height=2, command=self.on_snip
+        )
+        self.snip_button.grid(row=0, column=2, padx=5, pady=5)
+
+        self.help_button = tk.Button(
+            self.button_frame, text="Help", width=12, height=2, command=self.on_help
+        )
+        self.help_button.grid(row=0, column=3, padx=5, pady=5)
+
+        self.root.mainloop()
+
+    def updatephoto(self):
+        self.image = self.image.resize((500, 125))
+        self.photo = ImageTk.PhotoImage(self.image)
+        self.image_label.config(image=self.photo)
+
+    def on_copy_latex(self):
+        print("Copy LaTeX command executed.")
+        pyperclip.copy(self.latextext)
+
+    def on_copy_mathtype(self):
+        print("Copy MathType command executed.")
+        pyperclip.copy(self.mathtypetext)
+
+    def on_snip(self):
+        ScreenShot(main=self)
+        print("Start cal")
+        self.latextext = self.ocr.predict(self.image)["formula"]
+        self.mathtypetext = utils.postprocessForMathml(
+            latex2mathml.converter.convert(self.latextext)
+        )
+        self.updatephoto()
+        print("Snip command executed.")
+
+
+
+    def on_help(self):
+        top = tk.Toplevel()
+        top.title("Help")
+
+        msg = tk.Message(
+            top,
+            width=410,
+            text="点击snipping，鼠标左键选择截图区域，Enter键确定，Esc键退出。完成识别后，显示的图片会变化，此时即可以复制Latex或MathML格式的公式。\n满意的话请到Github给个赞，后续改进也会在上面发布：\n https://github.com/ai25395/FMatPix ",
+        )
+        msg.pack()
+
+    def run(self):
+        self.root.mainloop()
+
+
+if __name__ == "__main__":
+    if getattr(sys, "frozen", None):
+        basedir = sys._MEIPASS
+    else:
+        basedir = os.path.dirname(__file__)
+    model = models.Latex_OCR()
+    MainWindow(os.path.join(basedir, "models/GuiStartImg.png"), model)
---
+++ utils.py
@@ -0,0 +1,362 @@
+import cv2
+import numpy as np
+from PIL import Image
+import traceback
+from pathlib import Path
+from typing import List
+from typing import Tuple, Union
+from onnxruntime import GraphOptimizationLevel, InferenceSession, SessionOptions
+
+def postprocessForMathml(mathml):
+    if mathml.__contains__(
+        r'<mo stretchy="true" fence="true" form="prefix">&#x0007B;</mo>'
+    ):
+        mathml = mathml.replace(
+            r'<mo stretchy="true" fence="true" form="prefix">&#x0007B;</mo>',
+            r'<mfenced close="" open="{">',
+        )
+        mathml = mathml.replace(
+            r'<mo stretchy="true" fence="true" form="postfix" />', r"</mfenced>"
+        )
+    return mathml
+
+def token2str(tokens, tokenizer) -> list:
+    if len(tokens.shape) == 1:
+        tokens = tokens[None, :]
+    dec = [tokenizer.decode(tok, clean_up_tokenization_spaces=False) for tok in tokens]
+    return [
+        "".join(detok.split(" "))
+        .replace("Ġ", " ")
+        .replace("Ċ", " ")
+        .replace("[EOS]", "")
+        .replace("[BOS]", "")
+        .replace("[PAD]", "")
+        .strip()
+        for detok in dec
+    ]
+
+
+class PreProcess:
+    def __init__(self, detect_path, max_dims: List[int], min_dims: List[int]):
+        self.max_dims, self.min_dims = max_dims, min_dims
+        self.mean = np.array([0.7931, 0.7931, 0.7931]).astype(np.float32)
+        self.std = np.array([0.1738, 0.1738, 0.1738]).astype(np.float32)
+
+    def letterbox(
+        self,
+        im,
+        new_shape=(128, 640),
+        color=(114, 114, 114),
+        auto=False,
+        scaleFill=False,
+        scaleup=True,
+        stride=32,
+    ):
+        # Resize and pad image while meeting stride-multiple constraints
+        shape = im.shape[:2]  # current shape [height, width]
+        # if shape[0] > shape[1]:
+        #     max_wh = max(new_shape[0], new_shape[1])
+        #     new_shape = (max_wh, max_wh)
+        if isinstance(new_shape, int):
+            new_shape = (new_shape, new_shape)
+
+        # Scale ratio (new / old)
+        r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+        if not scaleup:  # only scale down, do not scale up (for better val mAP)
+            r = min(r, 1.0)
+
+        # Compute padding
+        ratio = r, r  # width, height ratios
+        new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+        dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+        if auto:  # minimum rectangle
+            dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+        elif scaleFill:  # stretch
+            dw, dh = 0.0, 0.0
+            new_unpad = (new_shape[1], new_shape[0])
+            ratio = (
+                new_shape[1] / shape[1],
+                new_shape[0] / shape[0],
+            )  # width, height ratios
+
+        dw /= 2  # divide padding into 2 sides
+        dh /= 2
+
+        if shape[::-1] != new_unpad:  # resize
+            im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
+        top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+        left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+        im = cv2.copyMakeBorder(
+            im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color
+        )  # add border
+        # return im, ratio, (dw, dh)
+
+        return im
+
+    def pad(self, img: Image.Image, divable: int = 32) -> Image.Image:
+        """Pad an Image to the next full divisible value of `divable`. Also normalizes the image and invert if needed.
+
+        Args:
+            img (PIL.Image): input image
+            divable (int, optional): . Defaults to 32.
+
+        Returns:
+            PIL.Image
+        """
+        threshold = 128
+        data = np.array(img.convert("LA"))
+        if data[..., -1].var() == 0:
+            data = (data[..., 0]).astype(np.uint8)
+        else:
+            data = (255 - data[..., -1]).astype(np.uint8)
+
+        data = (data - data.min()) / (data.max() - data.min()) * 255
+        if data.mean() > threshold:
+            # To invert the text to white
+            gray = 255 * (data < threshold).astype(np.uint8)
+        else:
+            gray = 255 * (data > threshold).astype(np.uint8)
+            data = 255 - data
+
+        coords = cv2.findNonZero(gray)  # Find all non-zero points (text)
+        a, b, w, h = cv2.boundingRect(coords)  # Find minimum spanning bounding box
+        rect = data[b : b + h, a : a + w]
+        im = Image.fromarray(rect).convert("L")
+        dims: List[Union[int, int]] = []
+        for x in [w, h]:
+            div, mod = divmod(x, divable)
+            dims.append(divable * (div + (1 if mod > 0 else 0)))
+
+        padded = Image.new("L", tuple(dims), 255)
+        padded.paste(im, (0, 0, im.size[0], im.size[1]))
+        return padded
+
+    def minmax_size(
+        self,
+        img: Image.Image,
+    ) -> Image.Image:
+        """Resize or pad an image to fit into given dimensions
+
+        Args:
+            img (Image): Image to scale up/down.
+
+        Returns:
+            Image: Image with correct dimensionality
+        """
+        if self.max_dims is not None:
+            ratios = [a / b for a, b in zip(img.size, self.max_dims)]
+            if any([r > 1 for r in ratios]):
+                size = np.array(img.size) // max(ratios)
+                img = img.resize(size.astype(int), Image.BILINEAR)
+
+        if self.min_dims is not None:
+            padded_size: List[Union[int, int]] = [
+                max(img_dim, min_dim)
+                for img_dim, min_dim in zip(img.size, self.min_dims)
+            ]
+
+            new_pad_size = tuple(padded_size)
+            if new_pad_size != img.size:  # assert hypothesis
+                padded_im = Image.new("L", new_pad_size, 255)
+                padded_im.paste(img, img.getbbox())
+                img = padded_im
+        return img
+
+    def normalize(self, img: np.ndarray, max_pixel_value=255.0) -> np.ndarray:
+        mean = self.mean * max_pixel_value
+        std = self.std * max_pixel_value
+        denominator = np.reciprocal(std, dtype=np.float32)
+        img = img.astype(np.float32)
+        img -= mean
+        img *= denominator
+        return img
+
+    @staticmethod
+    def to_gray(img) -> np.ndarray:
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        return cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
+
+    @staticmethod
+    def transpose_and_four_dim(img: np.ndarray) -> np.ndarray:
+        return img.transpose(2, 0, 1)[:1][None, ...]
+
+
+class OrtInferSession:
+    def __init__(self, model_path: Union[str, Path], num_threads: int = 1):
+        self.verify_exist(model_path)
+
+        self.num_threads = num_threads
+        self._init_sess_opt()
+
+        EP_list = ["CUDAExecutionProvider", "CPUExecutionProvider"]
+
+        try:
+            self.session = InferenceSession(
+                str(model_path), sess_options=self.sess_opt, providers=EP_list
+            )
+        except TypeError:
+            self.session = InferenceSession(str(model_path), sess_options=self.sess_opt)
+
+    def _init_sess_opt(self):
+        self.sess_opt = SessionOptions()
+        self.sess_opt.log_severity_level = 4
+        self.sess_opt.enable_cpu_mem_arena = False
+
+        if self.num_threads != -1:
+            self.sess_opt.intra_op_num_threads = self.num_threads
+
+        self.sess_opt.graph_optimization_level = GraphOptimizationLevel.ORT_ENABLE_ALL
+
+    def __call__(self, input_content: List[np.ndarray]) -> np.ndarray:
+        input_dict = dict(zip(self.get_input_names(), input_content))
+        try:
+            return self.session.run(None, input_dict)
+        except Exception as e:
+            error_info = traceback.format_exc()
+            raise ONNXRuntimeError(error_info) from e
+
+    def get_input_names(
+        self,
+    ):
+        return [v.name for v in self.session.get_inputs()]
+
+    def get_output_name(self, output_idx=0):
+        return self.session.get_outputs()[output_idx].name
+
+    def get_metadata(self):
+        meta_dict = self.session.get_modelmeta().custom_metadata_map
+        return meta_dict
+
+    @staticmethod
+    def verify_exist(model_path: Union[Path, str]):
+        if not isinstance(model_path, Path):
+            model_path = Path(model_path)
+
+        if not model_path.exists():
+            raise FileNotFoundError(f"{model_path} does not exist!")
+
+        if not model_path.is_file():
+            raise FileExistsError(f"{model_path} must be a file")
+
+
+class Decoder:
+    def __init__(self, decoder_path: Union[Path, str], max_seq_len=512):
+        self.max_seq_len = max_seq_len
+        self.session = OrtInferSession(decoder_path)
+
+    def __call__(
+        self,
+        start_tokens,
+        seq_len=256,
+        eos_token=None,
+        temperature=0.00001,
+        filter_thres=0.9,
+        context=None,
+    ):
+        num_dims = len(start_tokens.shape)
+
+        b, t = start_tokens.shape
+
+        out = start_tokens
+
+        confidences = []
+        for _ in range(seq_len):
+            x = out[:, -self.max_seq_len :]
+            ort_outs = self.session([x.astype(np.int64), context])[0]
+            np_preds = ort_outs
+            np_logits = np_preds[:, -1, :]
+            sample = np.argmax(np_logits, axis=-1)
+            np_probs = self.softmax(np_logits, axis=-1)
+            confidence = np.max(np_probs, axis=1)
+            confidences.append(confidence)
+            sample = np.expand_dims(sample, axis=-1)
+            out = np.concatenate([out, sample], axis=-1)
+
+            if (
+                eos_token is not None
+                and (np.cumsum(out == eos_token, axis=1)[:, -1] >= 1).all()
+            ):
+                break
+
+        out = out[:, t:]
+        if num_dims == 1:
+            out = out.squeeze(0)
+        return (out, [round(np.mean(i) * 100, 2) for i in confidences])
+
+    @staticmethod
+    def softmax(x, axis=None) -> float:
+        def logsumexp(a, axis=None, b=None, keepdims=False):
+            a_max = np.amax(a, axis=axis, keepdims=True)
+
+            if a_max.ndim > 0:
+                a_max[~np.isfinite(a_max)] = 0
+            elif not np.isfinite(a_max):
+                a_max = 0
+
+            tmp = np.exp(a - a_max)
+
+            # suppress warnings about log of zero
+            with np.errstate(divide="ignore"):
+                s = np.sum(tmp, axis=axis, keepdims=keepdims)
+                out = np.log(s)
+
+            if not keepdims:
+                a_max = np.squeeze(a_max, axis=axis)
+            out += a_max
+            return out
+
+        return np.exp(x - logsumexp(x, axis=axis, keepdims=True))
+
+    def npp_top_k(self, logits, thres=0.9):
+        k = int((1 - thres) * logits.shape[-1])
+        val, ind = self.np_top_k(logits, k)
+        probs = np.full_like(logits, float("-inf"))
+        np.put_along_axis(probs, ind, val, axis=1)
+        return probs
+
+    @staticmethod
+    def np_top_k(
+        a: np.ndarray, k: int, axis=-1, largest=True, sorted=True
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        if axis is None:
+            axis_size = a.size
+        else:
+            axis_size = a.shape[axis]
+
+        assert 1 <= k <= axis_size
+
+        a = np.asanyarray(a)
+        if largest:
+            index_array = np.argpartition(a, axis_size - k, axis=axis)
+            topk_indices = np.take(index_array, -np.arange(k) - 1, axis=axis)
+        else:
+            index_array = np.argpartition(a, k - 1, axis=axis)
+            topk_indices = np.take(index_array, np.arange(k), axis=axis)
+
+        topk_values = np.take_along_axis(a, topk_indices, axis=axis)
+        if sorted:
+            sorted_indices_in_topk = np.argsort(topk_values, axis=axis)
+            if largest:
+                sorted_indices_in_topk = np.flip(sorted_indices_in_topk, axis=axis)
+            sorted_topk_values = np.take_along_axis(
+                topk_values, sorted_indices_in_topk, axis=axis
+            )
+            sorted_topk_indices = np.take_along_axis(
+                topk_indices, sorted_indices_in_topk, axis=axis
+            )
+            return sorted_topk_values, sorted_topk_indices
+        return topk_values, topk_indices
+
+    @staticmethod
+    def multinomial(weights, num_samples, replacement=True):
+        weights = np.asarray(weights)
+        weights /= np.sum(weights)  # 确保权重之和为1
+        indices = np.arange(len(weights))
+        samples = np.random.choice(
+            indices, size=num_samples, replace=replacement, p=weights
+        )
+        return samples
+
+
+class ONNXRuntimeError(Exception):
+    pass
